95e2902a769157dfa8f05d3a6550bc33133ebe12
lmay <lmay@lmaye.com>
2019-12-06 14:40:01 +0800

    -- modify rabbitmq.

--- rabbitmq/init.sh
+++ rabbitmq/init.sh
@@ -26,13 +26,6 @@ if [ -d "./rabbitmq/" ]; then
 chmod 777 ./rabbitmq/data/
 fi
 echo -e "${BLUE_COLOR}===> directory authorize success.${RES}"
-
-# 移动配置文件
-echo -e "${BLUE_COLOR}---> move [rabbitmq]config file start.${RES}"
-if [ -f "./rabbitmq.conf" ]; then
-mv ./rabbitmq.conf ./rabbitmq
-fi
-echo -e "${BLUE_COLOR}===> move config files success.${RES}"
 echo -e "${GREEN_COLOR}>>>>>>>>>>>>>>>>>> The End <<<<<<<<<<<<<<<<<<${RES}"
 
 # 部署项目
